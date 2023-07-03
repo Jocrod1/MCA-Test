8b19f8929fbbe0d6e0725b9e6f4f24e13495de55
José Pereira <jocrod.one@gmail.com>
2023-07-03 10:07:48 +0200

    fix: :bug: remove ComposeWithDevTools to Redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import { persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage'
 import persistStore from "redux-persist/es/persistStore";
@@ -15,7 +15,7 @@ const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = createStore(
     persistedReducer,
-    composeWithDevTools(applyMiddleware(thunk)),
+    applyMiddleware(thunk),
 );
 
 const persistor = persistStore(store);
