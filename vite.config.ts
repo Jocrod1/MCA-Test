43e4f0d602021bafe573c460f04be94d313f7119
José Pereira <jocrod.one@gmail.com>
2023-07-03 09:34:34 +0200

    chore: :rotating_light: build for prod & dev commands

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (mode === 'dev') {
+    return {
+      // dev specific config
+      plugins: [react()],
+      build: {
+        minify: false
+      }
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [react()]
+    }
+  }
+}
+)
