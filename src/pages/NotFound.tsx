fc6dcfb4ea3b5a298428a00015bd639644eebd75
José Pereira <jocrod.one@gmail.com>
2023-07-06 11:50:15 +0200

    fix: :bug: exclamation sign

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div className="flex flex-col h-full w-full items-center justify-center text-6xl font-bold ">
       <p className="">{'404'}</p>
-      <p className="">{'Not Found'}</p>
+      <p className="">{'Not Found!'}</p>
       <p>😶‍🌫️</p>
       <br />
       <br />
